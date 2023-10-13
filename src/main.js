32f8508f312f200049552b05af44d8e85bc22645
kami <lilmnm-kamikaze-@users.noreply.github.com>
2023-10-13 01:54:26 -0400

    fix: $#xD; string in config.xml file after save

--- src/main.js
+++ src/main.js
@@ -15,33 +15,36 @@ const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 const log = console.log;
 
-const xmlBuilder = new Builder({'newline': '\r\n'});
+const xmlBuilder = new Builder({ 'newline': '\r\n' });
 
 function checkTypeErrors(configPath, version, buildNumber) {
-    if(typeof configPath !== 'string') {
+    if (typeof configPath !== 'string') {
         throw TypeError('"configPath" argument must be a string');
     }
 
-    if(version && typeof version !== 'string') {
+    if (version && typeof version !== 'string') {
         throw TypeError('"version" argument must be a string');
     }
 
-    if(buildNumber && typeof buildNumber !== 'number') {
+    if (buildNumber && typeof buildNumber !== 'number') {
         throw TypeError('"buildNumber" argument must be an integer');
     }
 
-    if(buildNumber && buildNumber !== parseInt(buildNumber, 10)) {
+    if (buildNumber && buildNumber !== parseInt(buildNumber, 10)) {
         throw TypeError('"buildNumber" argument must be an integer');
     }
 }
 
 async function getXml(configPath, templatePath) {
     let configFile = '';
-    if(templatePath){
+    if (templatePath) {
         configFile = await readFile(templatePath, 'UTF-8');
     } else {
         configFile = await readFile(configPath, 'UTF-8');
     }
+
+    // Replace &#xD; with an empty string
+    configFile = configFile.replace(/&#xD;/g, '');
 
     return xml2js(configFile);
 }
@@ -58,25 +61,25 @@ function setAttributes(xml, version, buildNumber, android, ios, extra) {
     let newXml = xml;
     const el = newXml.plugin ? 'plugin' : 'widget';
 
-    if(version) {
+    if (version) {
         newXml[el].$.version = version;
     }
-    if(!buildNumber && el === 'widget') {
+    if (!buildNumber && el === 'widget') {
         const [major, minor, patch] = version.split('.');
         log('%s The version being saved to config file is %d.%d.%d.', chalk.green.bold('INFO!'), major, minor, patch);
         buildNumber = evaluate('(' + major + ' * 10000) + (' + minor + ' * 100) + (' + patch + ')');
         log('%s The The build numbe writen to config file is %d.', chalk.green.bold('INFO!'), buildNumber);
     }
-    if(extra) {
+    if (extra) {
         log('%s The extra tag is enabled changing {{cdvverctrl}} to %s in config file', chalk.green.bold('INFO!'), version);
         // {{cdvverctrl}} this is what we will look for to replace
         try {
             newXml = JSON.parse(JSON.stringify(newXml).replace('{{cdvverctrl}}', version));
-        } catch {
+        } catch (error) {
             log('%s You need to add {{cdvverctrl}} in config file to use the extra option!', chalk.red.bold('ERROR!'), version);
         }
     }
-    if(el === 'widget' && buildNumber) {
+    if (el === 'widget' && buildNumber) {
         if (android) {
             newXml.widget.$['android-versionCode'] = buildNumber;
         } else if (ios) {
@@ -92,10 +95,9 @@ function setAttributes(xml, version, buildNumber, android, ios, extra) {
 
 async function cdvVerCrtl({ configPath, version, buildNumber, android, ios, extra, template, plugin } = {}) {
     let cPath = '';
-    if(!plugin) {
-         cPath = configPath || './config.xml';
+    if (!plugin) {
+        cPath = configPath || './config.xml';
     } else {
-        
         if (buildNumber) {
             log('%s Build numbers will be ignored on cordova plugin projects.', chalk.orange.bold('WARNING!'));
         }
@@ -105,21 +107,15 @@ async function cdvVerCrtl({ configPath, version, buildNumber, android, ios, extr
             process.exit(1);
         }
     }
-
-    if(android && ios) {
-      console.error('%s Please only use one -a or -i options at a time. Don\'t use either to update both android and ios build numbers.', chalk.red.bold('ERROR!'));
-      process.exit(1);
+    if (android && ios) {
+        console.error('%s Please only use one -a or -i options at a time. Don\'t use either to update both android and ios build numbers.', chalk.red.bold('ERROR!'));
+        process.exit(1);
     }
     log('%s The template is %s.', chalk.green.bold('INFO!'), template);
-
     checkTypeErrors(cPath, version, buildNumber);
-
     const currentConfig = await getXml(cPath, template);
-
     const v = !version && !buildNumber ? await getVersionFromPackage(version) : version;
-
     const newConfig = setAttributes(currentConfig, v, buildNumber, android, ios, extra);
-
     const newData = xmlBuilder.buildObject(newConfig);
     return writeFile(cPath, newData, { encoding: 'UTF-8', flag: 'w' });
 }
